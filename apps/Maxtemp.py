53758f508952f5a0be79a884e720f452ec373b9c
Mig Shyaka <79544418+migshyaka@users.noreply.github.com>
2021-08-02 16:26:44 -0500

    ggg

--- apps/Maxtemp.py
+++ apps/Maxtemp.py
@@ -21,10 +21,10 @@ from app import app
 # data_path = Path(data_path)
 # df = pd.read_csv("../datasets/Maximum temperature.csv")
 # df.head()
-# PATH = pathlib.Path(__file__).parent
-# DATA_PATH = PATH.joinpath("../rwandahydroclimatology/datasets").resolve()
-# df = pd.read_csv(DATA_PATH.joinpath("Maximum temperature.csv"))
-df = pd.read_csv("rwandahydroclimatology/datasets/Maximum temperature.csv")
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../datasets").resolve()
+df = pd.read_csv(DATA_PATH.joinpath("Maximum temperature.csv"))
+
 
 
 # In[7]:
