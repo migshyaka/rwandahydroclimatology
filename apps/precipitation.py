052843d08343f143c3e673c412d784b7e3ec1fff
Mig Shyaka <79544418+migshyaka@users.noreply.github.com>
2021-08-02 16:40:35 -0500

    ttt

--- apps/precipitation.py
+++ apps/precipitation.py
@@ -8,11 +8,6 @@ from app import app
 import pathlib
 
 
-# par_dir = os.pardir
-# data_path = os.path.join(par_dir, "datasets")
-# #data_path = Path(data_path)
-# df = pd.read_csv("/Users/pntaganda2/Documents/summner2021/proj/datasets/Daily rainfall in mm.csv")
-
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../datasets").resolve()
 df = pd.read_csv(DATA_PATH.joinpath("Daily rainfall in mm.csv"))
